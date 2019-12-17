e332779df9d077ea9be9c9fb441464cd8cdc547a
ymz <2858392644@qq.com>
2019-12-17 15:56:36 +0800

    crm 功能完善_多环境打包配置

---
+++ src/main/java/com/shsxt/crm/db/dao/CustomerOrderMapper.java
@@ -0,0 +1,8 @@
+package com.shsxt.crm.db.dao;
+
+import com.shsxt.base.BaseMapper;
+import com.shsxt.crm.vo.CustomerOrder;
+
+public interface CustomerOrderMapper extends BaseMapper<CustomerOrder,Integer> {
+
+}
